0d2fde0781b3ee7a6be7a08f793671eeb1d1c57b
Simone Zugarini <zuga_simo@hotmail.it>
2018-09-18 17:00:13 +0200

    Modifica elaborazione di un nuovo numero

---
+++ AppCompiti/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCompiti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -407235;
+            Console.WriteLine($"Il valore di a è {a}");
+            short s = (short)a;
+            Console.WriteLine($"Il valore di s è {s} ");
+            Console.ReadLine();
+        }
+    }
+}
